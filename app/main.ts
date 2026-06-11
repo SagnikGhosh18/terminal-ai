62153b3013cd8e27a9c0edeebd871e982b924cdc
Sagnik <sagnik@Sagniks-MacBook-Air.local>
2026-06-12 01:28:44 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -62,6 +62,27 @@ async function main() {
         },
       },
     },
+    {
+      "type": "function",
+      "function": {
+        "name": "Write",
+        "description": "Write content to a file",
+        "parameters": {
+          "type": "object",
+          "required": ["file_path", "content"],
+          "properties": {
+            "file_path": {
+              "type": "string",
+              "description": "The path of the file to write to"
+            },
+            "content": {
+              "type": "string",
+              "description": "The content to write to the file"
+            }
+          }
+        }
+      }
+    }
   ];
 
   while (true) {
@@ -96,6 +117,15 @@ async function main() {
             role: "tool",
             tool_call_id: toolCall.id,
             content: fileContent,
+          });
+        } else if(functionName === "Write") {
+          const content = args.content;
+          const file_path = args.file_path;
+          fs.writeFileSync(file_path, content);
+          messages.push({
+            role: "tool",
+            tool_call_id: toolCall.id,
+            content: content,
           });
         }
       }
